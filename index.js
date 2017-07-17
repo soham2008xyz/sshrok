0acb775207efd840d92a710c49e6d076e3f780ad
Soham Banerjee <soham@renderbit.com>
2017-07-17 17:44:18 +0530

    Pushbullet notifications added

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 var ngrok = require('ngrok');
+var PushBullet = require('pushbullet');
+var pusher = new PushBullet(process.env.PUSHBULLET_TOKEN);
 
 ngrok.connect({
 	proto: 'tcp',
@@ -10,4 +14,13 @@ ngrok.connect({
 	}
 
 	console.log("Server started on " + url);
+	pusher.note({
+		"source_device_iden": process.env.PUSHBULLET_DEVICE
+	}, "Server Started", "Listening on " + url, function(error, response) {
+		if (error) {
+			console.error(error);
+		}
+
+		// console.log(response);
+	});
 }); // tcp://0.tcp.ngrok.io:48590 
